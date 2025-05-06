25d078dda5d489f566024e8099dd5e4d1d678661
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-05-06 15:20:57 +0900

    .

--- model-server/app/insert.py
+++ model-server/app/insert.py
@@ -8,10 +8,9 @@ from app.utils.csv_loader import insert_data_from_csv
 from app.database_connect import SessionLocal
 
 async def main():
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "models/data/processed_train_small.csv"))
     async with SessionLocal() as session:
-        await insert_data_from_csv(
-            "./models/data/processed_train_small.csv",
-            session
-        )
+        await insert_data_from_csv(csv_path, session)
+
 
 asyncio.run(main())
